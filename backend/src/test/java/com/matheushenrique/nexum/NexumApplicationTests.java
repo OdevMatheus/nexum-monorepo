be131edadccf80f3f467ff79ce792ec2101d6081
Matheus Henrique <matheusvita007@gmail.com>
2026-05-22 21:36:54 -0300

    test(ci): remove placeholder context load test

--- backend/src/test/java/com/matheushenrique/nexum/NexumApplicationTests.java
+++ backend/src/test/java/com/matheushenrique/nexum/NexumApplicationTests.java
@@ -1,13 +1,10 @@
 package com.matheushenrique.nexum;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class NexumApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
